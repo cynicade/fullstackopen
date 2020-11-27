11a1febd110a21793be8c8cfc5e3d4ccf8c9e038
Cyn de Roix <cynderoix@gmail.com>
2020-11-27 06:52:44 +0100

    Exercise 1.13

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,63 +1,32 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Statistic = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  );
-};
-
-const Statistics = (props) => {
-  const all = props.good + props.neutral + props.bad;
-
-  if (all === 0) {
-    return (
-      <>
-        <h1>statistics</h1>
-        <p>No feedback given</p>
-      </>
-    );
-  }
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
 
   return (
     <>
-      <h1>statistics</h1>
-      <table>
-        <tbody>
-          <Statistic text="good" value={props.good} />
-          <Statistic text="neutral" value={props.neutral} />
-          <Statistic text="bad" value={props.bad} />
-          <Statistic text="all" value={all} />
-          <Statistic text="average" value={(props.good - props.bad) / all} />
-          <Statistic text="positive" value={(props.good / all) * 100 + '%'} />
-        </tbody>
-      </table>
+      <p>{props.anecdotes[selected]}</p>
+      <button
+        onClick={() =>
+          setSelected(
+            Math.floor(Math.random() * Math.floor(props.anecdotes.length))
+          )
+        }
+      >
+        next anecdote
+      </button>
     </>
   );
 };
 
-const Button = ({ handler, text }) => {
-  return <button onClick={handler}>{text}</button>;
-};
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  return (
-    <>
-      <h1>give feedback</h1>
-      <Button handler={() => setGood(good + 1)} text="good" />
-      <Button handler={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handler={() => setBad(bad + 1)} text="bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
-    </>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
