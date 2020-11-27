ba435313915b47a362972b2af3cf76b0d413e62a
Cyn de Roix <cynderoix@gmail.com>
2020-11-27 06:22:30 +0100

    Exercise 1.8

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,22 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad;
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {all}</p>
+      <p>average {(props.good - props.bad) / all}</p>
+      <p>positive {(props.good / all) * 100} %</p>
+    </>
+  );
+};
+
 const Button = ({ handler, text }) => {
   return <button onClick={handler}>{text}</button>;
 };
@@ -11,21 +27,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const all = good + neutral + bad;
-
   return (
     <>
       <h1>give feedback</h1>
       <Button handler={() => setGood(good + 1)} text="good" />
       <Button handler={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handler={() => setBad(bad + 1)} text="bad" />
-      <h1>statisctics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(good - bad) / all}</p>
-      <p>positive {(good / all) * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
